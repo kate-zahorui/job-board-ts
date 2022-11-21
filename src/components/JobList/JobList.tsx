b89617810d8b7e930c420ea8fc4155394c58077a
kate-zahorui <kate.zahorui@gmail.com>
2022-11-21 08:42:07 +0200

    Updates JobList

--- src/components/JobList/JobList.tsx
+++ src/components/JobList/JobList.tsx
@@ -11,45 +11,48 @@ interface IProps {
 }
 
 const JobList: React.FunctionComponent<IProps> = ({ currentItems }) => {
+  if (!currentItems) {
+    return null;
+  }
+
   return (
     <ul className={s.list}>
-      {currentItems &&
-        currentItems.map((item: IItem) => (
-          <li key={item.id} className={s.item}>
-            <div className={s.item__photobox}>
-              <img
-                src={item.pictures[0]}
-                alt={`${item.name}`}
-                className={s.item__image}
-              />
-            </div>
+      {currentItems.map((item: IItem) => (
+        <li key={item.id} className={s.item}>
+          <div className={s.item__photobox}>
+            <img
+              src={item.pictures[0]}
+              alt={`${item.name}`}
+              className={s.item__image}
+            />
+          </div>
 
-            <div className={s.info}>
-              <div className={s.actions}>
-                <div className={s.actions__rating}></div>
+          <div className={s.info}>
+            <div className={s.actions}>
+              <div className={s.actions__rating}></div>
 
-                <div className={s.actions__sidebox}>
-                  <button className={s.actions__button}>
-                    <svg className={s.actions__icon} width="32" height="32">
-                      <use href={`${svg}#icon-saveBookmark`}></use>
-                    </svg>
-                  </button>
-                  <JobDate date={item.createdAt} />
-                </div>
-              </div>
-
-              <div className={s.text}>
-                <h2 className={s.text__title}>
-                  <Link to={`/${item.id}`} className={s.text__link}>
-                    {item.title}
-                  </Link>
-                </h2>
-                <p className={s.text__name}>{item.name}</p>
-                <p className={s.text__address}>{item.address}</p>
+              <div className={s.actions__sidebox}>
+                <button className={s.actions__button}>
+                  <svg className={s.actions__icon} width="32" height="32">
+                    <use href={`${svg}#icon-saveBookmark`}></use>
+                  </svg>
+                </button>
+                <JobDate date={item.createdAt} />
               </div>
             </div>
-          </li>
-        ))}
+
+            <div className={s.text}>
+              <h2 className={s.text__title}>
+                <Link to={`/${item.id}`} className={s.text__link}>
+                  {item.title}
+                </Link>
+              </h2>
+              <p className={s.text__name}>{item.name}</p>
+              <p className={s.text__address}>{item.address}</p>
+            </div>
+          </div>
+        </li>
+      ))}
     </ul>
   );
 };
